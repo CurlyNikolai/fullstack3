eb8acfe1471fe7dcf10e8a08ff5f74f574dbda5d
CurlyNikolai <emil.levo@outlook.com>
2022-10-21 16:28:47 +0300

    stuck on error handling middleware

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 var morgan = require('morgan')
 const cors = require('cors')
-
+require('dotenv').config()
+const Person = require('./models/person')
 const app = express()
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors())
-
 app.use(morgan(function (tokens, req, res) {
     
     var message = [
@@ -24,31 +24,21 @@ app.use(morgan(function (tokens, req, res) {
     return message
 }))
 
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    },
-]
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
@@ -57,15 +47,16 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        response.json(person)
-    }
-    else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            }
+            else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -89,20 +80,16 @@ app.post('/api/persons', (request, response) => {
             error: 'number missing'
         })
     }
-    if (persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
 
-    const person = {
-        id: Math.floor(Math.random() * 1000000000),
+    const person = new Person({
         name: body.name,
         number: body.number,
-    }
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
     
-    persons = persons.concat(person)
-    response.json(person)
 })
 
 const PORT = process.env.PORT || 3001
---
+++ mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log("not enough arguments!\n"+
+                "to display all persons: node mongo.js <passwd>\n"+
+                "to add person: node.mongo.js <passwd> <name> <number>")
+    process.exit()
+}
+
+// Define cmd-line arguments
+const passwd = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url = `mongodb+srv://fullstack:${passwd}@cluster0.blc9sbg.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose.connect(url)
+        .then(result => {
+            console.log('connected')
+        })
+        .then(() => {
+            // Add new person to phonebook
+            if (newName && newNumber) {
+                const person = new Person({
+                    name: newName,
+                    number: newNumber
+                })
+                return person.save().then(() => {
+                    console.log(`added ${newName} number ${newNumber} to phonebook`)
+                    return mongoose.connection.close()
+                })
+            }
+            // If no name nor number is give, display existing persons
+            if (!newName && !newNumber) {
+                console.log("phonebook:")
+                Person.find({}).then(result => {
+                    result.forEach(person => {
+                        console.log(person.name + " " + person.number)
+                    })
+                    return mongoose.connection.close()
+                })
+            }
+        })
+        .catch((err) => console.log(err))
